d23ade2c5a5e26c7464bec6cb96ecb796c5d1fec
felipenevess <felipe012neves@gmail.com>
2022-03-18 10:03:15 -0300

    feat: add model user

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,46 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
