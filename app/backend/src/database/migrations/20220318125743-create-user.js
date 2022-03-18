52ef960cc1b10d0c5558c8b3780d7fb177a16824
felipenevess <felipe012neves@gmail.com>
2022-03-18 09:59:09 -0300

    feat: add migration create-user

---
+++ app/backend/src/database/migrations/20220318125743-create-user.js
@@ -0,0 +1,31 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
