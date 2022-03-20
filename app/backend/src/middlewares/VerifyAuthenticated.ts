9d00a253dc1d286f0d83728099e9254192516e61
felipenevess <felipe012neves@gmail.com>
2022-03-20 12:39:20 -0300

    feat: add file userAuthenticated

---
+++ app/backend/src/middlewares/VerifyAuthenticated.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import AppError from '../errors/AppError';
+import authConfig from '../config/auth';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+function UserAuthenticated(req: Request, _res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token not found', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+  const { secret } = authConfig.jwt;
+
+  try {
+    const decoded = verify(token, secret);
+    const { sub } = decoded as ITokenPayload;
+
+    req.user = { id: sub };
+
+    next();
+  } catch {
+    throw new AppError('Token expired or invalid', 401);
+  }
+}
+
+export default UserAuthenticated;
