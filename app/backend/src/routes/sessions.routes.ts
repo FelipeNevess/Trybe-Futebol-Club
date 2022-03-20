cdf9fad370e801e04e8a0b922729fc838c178e8c
felipenevess <felipe012neves@gmail.com>
2022-03-20 12:37:21 -0300

    feat: create route login/validate

--- app/backend/src/routes/sessions.routes.ts
+++ app/backend/src/routes/sessions.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 
 import AuthenticateController from '../modules/futebol/useCase/sessions/AuthenticateController';
-import UserAuthenticated from '../middlewares/verifyAuthenticated';
+import UserAuthenticated from '../middlewares/VerifyAuthenticated';
+import User from '../database/models/User';
+import { IResponseSession } from '../modules/futebol/repositories/ISessionsRepositories';
 
 const sessionsRouter = Router();
 
-sessionsRouter.post('/', UserAuthenticated, (req, res) => AuthenticateController.handle(req, res));
+sessionsRouter.post('/', (req, res) => AuthenticateController.handle(req, res));
+
+sessionsRouter.get('/validate', UserAuthenticated, async (req, res) => {
+  const { id } = req.user;
+  const { role } = await User.findOne({ where: { id } }) as IResponseSession;
+
+  return res.status(200).send(role);
+});
 
 export default sessionsRouter;
