938b89eea356d449d35909dd9c3f70c7d28dd569
felipenevess <felipe012neves@gmail.com>
2022-03-20 12:28:40 -0300

    fix: rename statusCode for code

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -29,7 +29,7 @@ class App {
 
     this.app.use((err: Error, _request: Request, res: Response, _: NextFunction) => {
       if (err instanceof AppError) {
-        return res.status(err.statusCode).json({ message: err.message });
+        return res.status(err.code).json({ message: err.message });
       }
 
       return res.status(500).json({ status: 'error', message: 'Internal server error' });
