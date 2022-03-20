da16d96a32f66bd17500a8f88d037bf48ed27d81
felipenevess <felipe012neves@gmail.com>
2022-03-20 12:29:56 -0300

    refactor: refactoring read secret file

--- app/backend/src/config/auth.ts
+++ app/backend/src/config/auth.ts
@@ -1,8 +1,13 @@
-import readFile from './readAuth';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 export default {
   jwt: {
-    secret: readFile(),
+    secret:
+      readFileSync(
+        resolve(__dirname, '..', '..', 'jwt.evaluation.key'),
+        { encoding: 'utf8' },
+      ).trim(),
     expiresIn: '1d',
   },
 };
