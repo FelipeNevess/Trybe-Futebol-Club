7df5f976abc91d3528d097b541fa297129f1183b
felipenevess <felipe012neves@gmail.com>
2022-03-24 19:04:09 -0300

    nada

--- app/backend/src/routes/sessions.routes.ts
+++ app/backend/src/routes/sessions.routes.ts
@@ -11,7 +11,6 @@ sessionsRouter.post('/', (req, res) => AuthenticateController.handle(req, res));
 
 sessionsRouter.get('/validate', UserAuthenticated, async (req, res) => {
   const { id } = req.body.user;
-
   const { role } = await User.findOne({ where: { id } }) as IResponseSession;
 
   return res.status(200).json(role);
